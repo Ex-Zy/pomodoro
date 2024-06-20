d72ec7f3b34bdb86d8b48348dcfe62e46f6ea418
Sergey Nagornykh <serega1905@ukr.net>
2024-06-20 13:02:54 +0300

    :up: chore: remove unused import

--- src/components/PomodoroSettings/PomodoroSettings.tsx
+++ src/components/PomodoroSettings/PomodoroSettings.tsx
@@ -1,5 +1,4 @@
 import './PomodoroSettings.scss'
-import { ModalClose } from '@mui/joy'
 import type React from 'react'
 import { useRef, useState } from 'react'
 import { Transition } from 'react-transition-group'
